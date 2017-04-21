28f34b517cdf2b8d0a35dd33b7b2fa7fbfeef3ea
Jack Wu <xuwupeng2000@gmail.com>
2017-04-21 22:13:37 +1200

    Add more validation

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,10 @@
 class Team < ApplicationRecord
   has_many :quiz_sessions, dependent: :delete_all
   has_one :auth_token
+
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_numericality_of :pass
-
+  validates_length_of :pass, maximum: 4
+  validates_length_of :pass, minimum: 4
 end
