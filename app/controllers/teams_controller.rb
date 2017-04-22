1ad11bbed8dc810c8a2805bbe50020092842258a
Jack Wu <xuwupeng2000@gmail.com>
2017-04-22 12:32:32 +1200

    Fix update password and pass change issue

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -25,8 +25,9 @@ class TeamsController < ApplicationController
   def update
     @team = Team.find(params[:id])
 
-    if @team.update(name: team_params[:name], description: team_params[:description], password: team_params[:password])
-      redirect_to team_quiz_sessions_path(@team)
+    if @team.update(name: team_params[:name], description: team_params[:description], password: team_params[:pass_number])
+      flash[:notice] = 'You team detail has been updated'
+      redirect_to root_path
     else
       render :edit
     end
